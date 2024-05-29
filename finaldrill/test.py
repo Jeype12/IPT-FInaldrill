cd50ae1fb6e8257a2ddd6cf00ef464557a636948
Jeype12 <202280128@psu.palawan.edu.ph>
2024-05-29 12:47:23 +0800

    add test for new function

--- finaldrill/test.py
+++ finaldrill/test.py
@@ -5,19 +5,24 @@ import warnings
 class MyAppTest(unittest.TestCase):
     def setUp(self):
         app.config["Testing"] = True
-        self.app = app.test.client()
+        self.app = app.test_client()
 
-        warnings.simplefilter("ignore",category=DeprecationWarning)
+        warnings.simplefilter("ignore", category=DeprecationWarning)
 
     def test_index_page(self):
-        response =  self.app.get("/")
+        response = self.app.get("/")
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data.decode(),"<p>Hello, World!</p>")
+        self.assertEqual(response.data.decode(), "<p>Hello, World!</p>")
 
     def test_getemployee(self):
-        response = self.app.get("/")
-        self.assertEqual(response.status_code,200)
-        self.assertTrue("EMPLOYEE" in response.data.decode())
+        response = self.app.get("/employee")
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue("James Cruz" in response.data.decode())
 
-    if __name__ == "__main__":
-        unittest.main()
+    def test_getemployee_by_id(self):
+        response = self.app.get("/employee/2024003")
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue("Warney Manlimot" in response.data.decode())
+
+if __name__ == "__main__":
+    unittest.main()
